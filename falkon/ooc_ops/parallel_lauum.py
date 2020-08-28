Small fixes to parallel lauum:  - fix available memory calculation for cuda inputs  - fix unexpected ram usage with torch.copy_

falkon/ooc_ops/parallel_lauum.py
@@ -53,7 +53,6 @@ def par_lauum_f_lower(A: torch.Tensor,
 
     tc_device = torch.device('cuda:%d' % (device_id))
     s1 = torch.cuda.Stream(device=tc_device)
-    s2 = torch.cuda.Stream(device=tc_device)
     s3 = torch.cuda.Stream(device=tc_device)
     cublasSetStream(cublas_handle, s1._as_parameter_)
 
@@ -109,7 +108,7 @@ def par_lauum_f_lower(A: torch.Tensor,
                             cur_lauum_out.copy_(col_b[:bb.length, :bb.length])
 
                         # LAUUM on col_b[:bb.length, :bb.length], into lauum_out[:bb.length, :bb.length]
-                        cuda_lauum_lower(n=bb.length, A=col_b[:bb.length, :bb.length], lda=A.shape[0], B=cur_lauum_out, ldb=max_block_size)
+                        cuda_lauum_lower(n=bb.length, A=col_b[:bb.length, :bb.length], lda=col_b.stride(1), B=cur_lauum_out, ldb=max_block_size)
                     s3.synchronize()
 
                     # Add outputs of SYRK and LAUUM (only if SYRK was performed)
@@ -123,7 +122,7 @@ def par_lauum_f_lower(A: torch.Tensor,
                     if independent_output:
                         Abb.copy_(cur_lauum_out.T)
                     else:
-                        Abb.copy_(cur_lauum_out)
+                        copy_to_host(bb.length, bb.length, cur_lauum_out, 0, 0, Abb, 0, 0, s=s1)
                 elif r > b:
                     br = block_allocs[r]
 
@@ -168,9 +167,8 @@ def par_lauum_f_lower(A: torch.Tensor,
                             s1.synchronize()
                             A[bb.start:bb.end, br.start:br.end].copy_(_temp_cpu.T)
                     elif not is_cuda:
-                        s1.synchronize()
-                        copy_to_host(br.length, bb.length, ccb, 0, 0, A, br.start, bb.start, s2)
-            s2.synchronize()
+                        copy_to_host(br.length, bb.length, ccb, 0, 0, A, br.start, bb.start, s1)
+            s1.synchronize()
 
 
 def par_lauum_c_lower(A: torch.Tensor,
@@ -191,9 +189,8 @@ def par_lauum_c_lower(A: torch.Tensor,
 
     tc_device = torch.device('cuda:%d' % (device_id))
     s1 = torch.cuda.Stream(device=tc_device)
-    s2 = torch.cuda.Stream(device=tc_device)
     s3 = torch.cuda.Stream(device=tc_device)
-    s1_cuda, s2_cuda = s1._as_parameter_, s2._as_parameter_
+    s1_cuda = s1._as_parameter_
     cublasSetStream(cublas_handle, s1_cuda)
 
     max_block_size = max(ba.length for ba in block_allocs)
@@ -264,7 +261,13 @@ def par_lauum_c_lower(A: torch.Tensor,
                     if independent_output:
                         Abb.copy_(lauum_out)
                     else:
-                        Abb.copy_(lauum_out.T)
+                        if not is_cuda:
+                            # It is not possible to directly copy lauum_out.T to Abb due to mismatch in strides.
+                            # Use CPU buffer for copy, and then copy transposed into Abb
+                            temp_bb[:bb.length, :bb.length].copy_(lauum_out)
+                            Abb.copy_(temp_bb[:bb.length, :bb.length].T)
+                        else:
+                            Abb.copy_(lauum_out.T)
                 else:  # r > b
                     br = block_allocs[r]
 
@@ -312,10 +315,10 @@ def par_lauum_c_lower(A: torch.Tensor,
                         s1.synchronize()
                         A[bb.start:bb.end, br.start:br.end].copy_(temp_bb[:br.length, :bb.length].T)
                     else:
-                        s1.synchronize()
                         cublasGetMatrixAsync(
                             rows=bb.length, cols=br.length, elem_size=dts,
                             A=ccb.data_ptr(), lda=max_block_size,
                             B=A[br.start, bb.start].data_ptr(), ldb=A.shape[0],
-                            stream=s2_cuda)
-            s2.synchronize()
+                            stream=s1_cuda)
+            s1.synchronize()
+

falkon/tests/test_ooc_lauum.py
@@ -71,7 +71,7 @@ def matrix():
 @pytest.fixture(scope="module")
 def get_mat(matrix):
     def getter(order, dtype):
-        return fix_mat(matrix, dtype=dtype, order=order)
+        return fix_mat(matrix, dtype=dtype, order=order, copy=True)
 
     return getter
 
@@ -110,10 +110,12 @@ class TestOOCLauum:
 
         with memory_checker(self.basic_opt, extra_mem=mgpu_slack) as new_opt:
             act_up = gpu_lauum(mat, upper=True, overwrite=False, opt=new_opt)
+            torch.cuda.synchronize()
         np.testing.assert_allclose(expected_upper, act_up.cpu().numpy(), rtol=self.rtol[dtype])
 
         with memory_checker(self.basic_opt, extra_mem=mgpu_slack) as new_opt:
             act_lo = gpu_lauum(mat, upper=False, overwrite=False, opt=new_opt)
+            torch.cuda.synchronize()
         np.testing.assert_allclose(expected_lower, act_lo.cpu().numpy(), rtol=self.rtol[dtype])
 
     @pytest.mark.parametrize("dtype", [np.float32, np.float64])
@@ -136,6 +138,7 @@ class TestOOCLauum:
         mat = torch.from_numpy(omat.copy(order="K"))
         with memory_checker(self.basic_opt) as new_opt:
             act_up = gpu_lauum(mat, upper=True, overwrite=True, write_opposite=True, opt=new_opt)
+            torch.cuda.synchronize()
         np.testing.assert_allclose(np.triu(omat, k=1), np.triu(act_up.numpy(), k=1),
                                    rtol=self.rtol[dtype])
         np.testing.assert_allclose(np.tril(act_up.numpy()), np.triu(expected_upper).T,
@@ -144,18 +147,20 @@ class TestOOCLauum:
         mat = torch.from_numpy(omat.copy(order="K"))
         with memory_checker(self.basic_opt) as new_opt:
             act_lo = gpu_lauum(mat, upper=False, overwrite=True, write_opposite=True, opt=new_opt)
+            torch.cuda.synchronize()
         np.testing.assert_allclose(np.tril(omat, k=-1), np.tril(act_lo.numpy(), k=-1),
                                    rtol=self.rtol[dtype])
         np.testing.assert_allclose(np.triu(act_lo.numpy()), np.tril(expected_lower).T,
                                    rtol=self.rtol[dtype])
 
-    def test_no_blk_mul(self, get_mat, expected_lower):
+    def test_no_blk_mul(self, get_mat, expected_upper):
         dtype = np.float32
         mat = get_mat(order="F", dtype=dtype).numpy().copy(order="K")
         opt = dataclasses.replace(self.basic_opt, lauum_par_blk_multiplier=1)
 
-        act_lo = gpu_lauum(torch.from_numpy(mat), upper=False, overwrite=True, opt=opt)
-        np.testing.assert_allclose(expected_lower, act_lo.numpy(), rtol=self.rtol[dtype])
+        act_lo = gpu_lauum(torch.from_numpy(mat), upper=True, overwrite=True, opt=opt)
+        torch.cuda.synchronize()
+        np.testing.assert_allclose(expected_upper, act_lo.numpy(), rtol=self.rtol[dtype])
 
 
 @pytest.mark.skipif(not decide_cuda(), reason="No GPU found.")