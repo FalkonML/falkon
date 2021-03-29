539f8ad9ea7fcf25bf499fea7f22c9fdbb5d1146
giacomo <giacomo.meanti@edu.unige.it>
2021-03-29 15:32:44 +0200

    Fix stress-test

--- falkon/tests/test_stress_multi_core.py
+++ falkon/tests/test_stress_multi_core.py
@@ -18,32 +18,32 @@ from falkon.utils import decide_cuda
 
 def _runner_str(fname_X, fname_Y, fname_out, num_centers, num_rep, max_iter, gpu_num):
     run_str = f"""
-    import pickle
-    import torch
-    from falkon import kernels, FalkonOptions, InCoreFalkon
-    from falkon.center_selection import FixedSelector
-    
-    num_rep = 5
-    kernel = kernels.GaussianKernel(20.0)
-    with open('{fname_X}', 'rb') as fh:
-        X = pickle.load(fh)
-    with open('{fname_Y}', 'rb') as fh:
-        Y = pickle.load(fh)
-    X, Y = X.cuda(), Y.cuda()
-    
-    opt = FalkonOptions(use_cpu=False, keops_active="no", debug=False, #never_store_kernel=True,
-                        max_gpu_mem=1*2**30, cg_full_gradient_every=10,
-                        min_cuda_iter_size_32=0, min_cuda_iter_size_64=0, min_cuda_pc_size_32=0, min_cuda_pc_size_64=0)
-    out = []
-    for rep in range({num_rep}):
-        center_sel = FixedSelector(X[:{num_centers}])
-        flk = InCoreFalkon(
-            kernel=kernel, penalty=1e-6, M={num_centers}, seed=10, options=opt, maxiter={max_iter},
-            center_selection=center_sel)
-        flk.fit(X, Y)
-        out.append(flk.predict(X))
-    with open('{fname_out}', 'wb') as fh:
-        pickle.dump([o.cpu() for o in out], fh)
+import numpy
+import pickle
+import torch
+from falkon import kernels, FalkonOptions, InCoreFalkon
+from falkon.center_selection import FixedSelector
+
+kernel = kernels.GaussianKernel(20.0)
+with open('{fname_X}', 'rb') as fh:
+    X = pickle.load(fh)
+with open('{fname_Y}', 'rb') as fh:
+    Y = pickle.load(fh)
+X, Y = X.cuda(), Y.cuda()
+
+opt = FalkonOptions(use_cpu=False, keops_active="no", debug=False, #never_store_kernel=True,
+                    max_gpu_mem=1*2**30, cg_full_gradient_every=2,
+                    min_cuda_iter_size_32=0, min_cuda_iter_size_64=0, min_cuda_pc_size_32=0, min_cuda_pc_size_64=0)
+out = []
+for rep in range({num_rep}):
+    center_sel = FixedSelector(X[:{num_centers}])
+    flk = InCoreFalkon(
+        kernel=kernel, penalty=1e-6, M={num_centers}, seed=10, options=opt, maxiter={max_iter},
+        center_selection=center_sel)
+    flk.fit(X, Y)
+    out.append(flk.predict(X))
+with open('{fname_out}', 'wb') as fh:
+    pickle.dump([o.cpu() for o in out], fh)
     """
     # Save string to temporary file
     py_fname = f"./temp_runner_gpu{gpu_num}_{random.randint(0, 1000)}.py"
@@ -59,7 +59,8 @@ class TestStressInCore:
     def test_multiple_falkons(self):
         num_processes = 6
         num_rep = 5
-        max_iter = 15
+        max_iter = 20
+        num_centers = 1000
         num_gpus = torch.cuda.device_count()
         X, Y = datasets.make_regression(50000, 2048, random_state=11)
         X = torch.from_numpy(X.astype(np.float32))
@@ -86,7 +87,8 @@ class TestStressInCore:
                                          'fname_out': out_files[i],
                                          'num_rep': num_rep,
                                          'max_iter': max_iter,
-                                         'gpu_num': i % num_gpus
+                                         'num_centers': num_centers,
+                                         'gpu_num': i % num_gpus,
                                      },
                                      daemon=False)
                 threads.append(t)
@@ -95,27 +97,30 @@ class TestStressInCore:
             for t in threads:
                 t.join()
 
+            # Load outputs
+            actual = []
+            for of in out_files:
+                with open(of, 'rb') as fh:
+                    actual.append(pickle.load(fh))
+
         # Expected result
         kernel = kernels.GaussianKernel(20.0)
         X, Y = X.cuda(), Y.cuda()
         opt = FalkonOptions(use_cpu=False, keops_active="no", debug=False, #never_store_kernel=True,
-                            max_gpu_mem=1*2**30, cg_full_gradient_every=10)
-        center_sel = FixedSelector(X[:2000])
+                            max_gpu_mem=1*2**30, cg_full_gradient_every=2)
+        center_sel = FixedSelector(X[:num_centers])
         flk = InCoreFalkon(
-            kernel=kernel, penalty=1e-6, M=2000, seed=10, options=opt,
-            maxiter=15, center_selection=center_sel)
+            kernel=kernel, penalty=1e-6, M=num_centers, seed=10, options=opt,
+            maxiter=max_iter, center_selection=center_sel)
         flk.fit(X, Y)
         expected = flk.predict(X).cpu()
-        # Load outputs
-        actual = []
-        for of in out_files:
-            with open(of, 'rb') as fh:
-                actual.append(pickle.load(fh))
         # Compare actual vs expected
         wrong = 0
         for i in range(len(actual)):
-            try:
-                np.testing.assert_allclose(expected.numpy(), actual[i].numpy(), rtol=1e-7)
-            except:
-                wrong += 1
+            for j in range(len(actual[i])):
+                try:
+                    np.testing.assert_allclose(expected.numpy(), actual[i][j].numpy(), rtol=1e-7)
+                except:
+                    print(f"Result {j} from process {i} is incorrect")
+                    wrong += 1
         assert wrong == 0, "%d results were not equal" % (wrong)
