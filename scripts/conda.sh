a6c2b2dc9a7c380febd00c394c2161a37adcf138
gmeanti <giacomo.meanti@gmail.com>
2021-04-19 20:55:31 +0200

    Fix another typo

--- scripts/conda.sh
+++ scripts/conda.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
 
-conda create --yes -n "${CONDA_ENV}" python="${PYTHON_VERSION}"
+conda create --yes -n "${CONDA_ENV}" python="${PY_VERSION}"
 source activate "${CONDA_ENV}"
