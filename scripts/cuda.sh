3c69e0a6bee54a186bbe95d239df6b212305ce94
gmeanti <giacomo.meanti@gmail.com>
2021-04-21 13:35:05 +0200

    Maybe 102 is not special?

--- scripts/cuda.sh
+++ scripts/cuda.sh
@@ -10,12 +10,12 @@ fi
 if [ "${CUDA_VERSION}" = "102" ]; then
   TOOLKIT_PATH="/opt/cuda10.2"
   # Fix cublas on CUDA 10.2:
-  if [ -d "${TOOLKIT_PATH}/targets/x86_64-linux/include" ]; then
-    cp -r $TOOLKIT_PATH/targets/x86_64-linux/include/* "${TOOLKIT_PATH}/include/"
-  fi
-  if [ -d "${TOOLKIT_PATH}/targets/x86_64-linux/lib" ]; then
-    cp -r $TOOLKIT_PATH/targets/x86_64-linux/lib/* "${TOOLKIT_PATH}/lib/"
-  fi
+#  if [ -d "${TOOLKIT_PATH}/targets/x86_64-linux/include" ]; then
+#    cp -r $TOOLKIT_PATH/targets/x86_64-linux/include/* "${TOOLKIT_PATH}/include/"
+#  fi
+#  if [ -d "${TOOLKIT_PATH}/targets/x86_64-linux/lib" ]; then
+#    cp -r $TOOLKIT_PATH/targets/x86_64-linux/lib/* "${TOOLKIT_PATH}/lib/"
+#  fi
 fi
 if [ "${CUDA_VERSION}" = "110" ]; then
   TOOLKIT_PATH="/opt/cuda11.0"
