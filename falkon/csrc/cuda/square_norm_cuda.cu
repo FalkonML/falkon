83d03cf2bfc5c6f28922881eee59539b41cf557f
gmeanti <giacomo.meanti@gmail.com>
2021-04-25 22:58:18 +0200

    C file debug

--- falkon/csrc/cuda/square_norm_cuda.cu
+++ falkon/csrc/cuda/square_norm_cuda.cu
@@ -2,6 +2,7 @@
 #include <ATen/ATen.h>
 #include <ATen/native/ReduceOpsUtils.h>
 #include <ATen/native/cuda/Reduce.cuh>
+#include <c10/cuda/CUDACachingAllocator.h>
 
 #include "square_norm_cuda.h"
 #include "../util.h"
@@ -16,17 +17,31 @@ void square_vector_norm_cuda_impl(at::TensorIterator& iter) {
 }
 
 torch::Tensor square_norm_cuda(const torch::Tensor& input, int64_t dim, torch::optional<bool> opt_keepdim) {
+static_cast<size_t>(StatType::AGGREGATE)]
+    auto device_stats = c10::cuda::CUDACachingAllocator::getDeviceStats(input.device().index());
+    printf("At beginning of cuda sq-norm: %.4fMB\n", device_stats.allocated_bytes[static_cast<size_t>(
+        c10::cuda::CUDACachingAllocator::StatType::AGGREGATE)].peak);
     at::IntArrayRef dimArr = at::IntArrayRef(dim);
     at::ScalarType in_dtype = input.scalar_type();
     bool keepdim = opt_keepdim.value_or(false);
 
     // Create the output tensor
     auto result_shape = shape_from_dim(input, dim, keepdim);
+    device_stats = c10::cuda::CUDACachingAllocator::getDeviceStats(input.device().index());
+    printf("Before output creation: %.4fMB\n", device_stats.allocated_bytes[static_cast<size_t>(
+        c10::cuda::CUDACachingAllocator::StatType::AGGREGATE)].peak);
     torch::Tensor result = torch::empty(result_shape, input.options());
+    printf("After output creation: %.4fMB\n", device_stats.allocated_bytes[static_cast<size_t>(
+        c10::cuda::CUDACachingAllocator::StatType::AGGREGATE)].peak);
+
     at::TensorIterator iter = at::native::make_reduction("vector_sqnorm", result, input, dimArr, keepdim, in_dtype);
+    printf("After iterator creation: %.4fMB\n", device_stats.allocated_bytes[static_cast<size_t>(
+        c10::cuda::CUDACachingAllocator::StatType::AGGREGATE)].peak);
     AT_DISPATCH_FLOATING_TYPES(iter.input_dtype(), "square_vector_norm_cuda", [&] {
         square_vector_norm_cuda_impl<scalar_t>(iter);
     });
+    printf("At end: %.4fMB\n", device_stats.allocated_bytes[static_cast<size_t>(
+        c10::cuda::CUDACachingAllocator::StatType::AGGREGATE)].peak);
     return result;
 }
 
