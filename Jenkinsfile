68da2421396991b0c05d3587ec11cf5b849dea34
Giacomo Meanti <giacomo.meanti@gmail.com>
2021-04-30 12:04:09 +0200

    Fix path names

--- Jenkinsfile
+++ Jenkinsfile
@@ -30,7 +30,7 @@ def setupCuda(start_path) {
         script: 'bash ./scripts/cuda.sh'
     ).trim()
     env.CUDA_HOME = "${toolkit_path}"
-    env.LD_LIBRARY_PATH = "${toolkit_path}/lib64/:${toolkit_path}/extras/CUPTI/lib64"
+    env.LD_LIBRARY_PATH = "${toolkit_path}/lib64/:${toolkit_path}/extras/CUPTI/lib64:/opt/rh/devtoolset-7/root/usr/lib64:/opt/rh/devtoolset-7/root/usr/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64"
     return "${toolkit_path}/bin:${start_path}"
 }
 
--- scripts/cuda.sh
+++ scripts/cuda.sh
@@ -2,19 +2,19 @@
 set -euxo pipefail
 
 if [ "${CUDA_VERSION}" = "92" ]; then
-  TOOLKIT_PATH="/usr/local/cuda9.2"
+  TOOLKIT_PATH="/usr/local/cuda-9.2"
 fi
 if [ "${CUDA_VERSION}" = "101" ]; then
-  TOOLKIT_PATH="/usr/local/cuda10.1"
+  TOOLKIT_PATH="/usr/local/cuda-10.1"
 fi
 if [ "${CUDA_VERSION}" = "102" ]; then
-  TOOLKIT_PATH="/usr/local/cuda10.2"
+  TOOLKIT_PATH="/usr/local/cuda-10.2"
 fi
 if [ "${CUDA_VERSION}" = "110" ]; then
-  TOOLKIT_PATH="/usr/local/cuda11.0"
+  TOOLKIT_PATH="/usr/local/cuda-11.0"
 fi
 if [ "${CUDA_VERSION}" = "111" ]; then
-  TOOLKIT_PATH="/usr/local/cuda11.1"
+  TOOLKIT_PATH="/usr/local/cuda-11.1"
 fi
 
 echo "${TOOLKIT_PATH}"
