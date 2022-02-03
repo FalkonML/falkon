77e74b5f8b4cd1b0f13c5325b79846fd26795edf
gmeanti <giacomo.meanti@gmail.com>
2022-02-03 14:41:25 +0100

    Refactor cuda utils

--- falkon/csrc/cuda/utils.cuh
+++ falkon/csrc/cuda/utils.cuh
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <torch/extension.h>
+#include <cusolverDn.h>
+#include <cublas_v2.h>
 
 #define CHECK_CUDA(x) TORCH_CHECK(x.device().is_cuda(), #x " must be a CUDA tensor.")
 #define CHECK_CPU(x) TORCH_CHECK(!x.device().is_cuda(), #x " must be a CPU tensor.")
@@ -34,5 +36,87 @@ inline int ceildiv(int dividend, int divisor) {
 }
 
 
+#define TORCH_CUSOLVER_CHECK(EXPR)                              \
+  do {                                                          \
+    cusolverStatus_t __err = EXPR;                              \
+    TORCH_CHECK(__err == CUSOLVER_STATUS_SUCCESS,               \
+                "CUDA error: ",                                 \
+                cusolverGetErrorString(__err),                  \
+                " when calling `" #EXPR "`");                   \
+  } while (0)
 
+const char* cusolverGetErrorString(cusolverStatus_t error) {
+  if (error == CUSOLVER_STATUS_SUCCESS) {
+    return "CUBLAS_STATUS_SUCCESS";
+  }
+  if (error == CUSOLVER_STATUS_NOT_INITIALIZED) {
+    return "CUSOLVER_STATUS_NOT_INITIALIZED";
+  }
+  if (error == CUSOLVER_STATUS_ALLOC_FAILED) {
+    return "CUSOLVER_STATUS_ALLOC_FAILED";
+  }
+  if (error == CUSOLVER_STATUS_INVALID_VALUE) {
+    return "CUSOLVER_STATUS_INVALID_VALUE";
+  }
+  if (error == CUSOLVER_STATUS_ARCH_MISMATCH) {
+    return "CUSOLVER_STATUS_ARCH_MISMATCH";
+  }
+  if (error == CUSOLVER_STATUS_EXECUTION_FAILED) {
+    return "CUSOLVER_STATUS_EXECUTION_FAILED";
+  }
+  if (error == CUSOLVER_STATUS_INTERNAL_ERROR) {
+    return "CUSOLVER_STATUS_INTERNAL_ERROR";
+  }
+  if (error == CUSOLVER_STATUS_MATRIX_TYPE_NOT_SUPPORTED) {
+    return "CUSOLVER_STATUS_MATRIX_TYPE_NOT_SUPPORTED";
+  }
+  return "<unknown>";
+}
+
+
+#define TORCH_CUDABLAS_CHECK(EXPR)                                      \
+  do {                                                          \
+    cublasStatus_t __err = EXPR;                              \
+    TORCH_CHECK(__err == CUBLAS_STATUS_SUCCESS,                 \
+                "CuBLAS error: ",                               \
+                cublasGetErrorString(__err),                    \
+                " when calling `" #EXPR "`");                   \
+  } while (0)
+
+
+const char* cublasGetErrorString(cublasStatus_t error) {
+  if (error == CUBLAS_STATUS_SUCCESS) {
+    return "CUBLAS_STATUS_SUCCESS";
+  }
+  if (error == CUBLAS_STATUS_NOT_INITIALIZED) {
+    return "CUBLAS_STATUS_NOT_INITIALIZED";
+  }
+  if (error == CUBLAS_STATUS_ALLOC_FAILED) {
+    return "CUBLAS_STATUS_ALLOC_FAILED";
+  }
+  if (error == CUBLAS_STATUS_INVALID_VALUE) {
+    return "CUBLAS_STATUS_INVALID_VALUE";
+  }
+  if (error == CUBLAS_STATUS_ARCH_MISMATCH) {
+    return "CUBLAS_STATUS_ARCH_MISMATCH";
+  }
+  if (error == CUBLAS_STATUS_MAPPING_ERROR) {
+    return "CUBLAS_STATUS_MAPPING_ERROR";
+  }
+  if (error == CUBLAS_STATUS_EXECUTION_FAILED) {
+    return "CUBLAS_STATUS_EXECUTION_FAILED";
+  }
+  if (error == CUBLAS_STATUS_INTERNAL_ERROR) {
+    return "CUBLAS_STATUS_INTERNAL_ERROR";
+  }
+  if (error == CUBLAS_STATUS_NOT_SUPPORTED) {
+    return "CUBLAS_STATUS_NOT_SUPPORTED";
+  }
+#ifdef CUBLAS_STATUS_LICENSE_ERROR
+  if (error == CUBLAS_STATUS_LICENSE_ERROR) {
+    return "CUBLAS_STATUS_LICENSE_ERROR";
+  }
+#endif
+  return "<unknown>";
+}
 
