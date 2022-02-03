06edca6da418c8c6d8122bff587ff446250b6bf0
Giacomo Meanti <giacomo.meanti@gmail.com>
2022-02-03 14:26:29 +0000

    Added more bindings[WIP]

--- falkon/csrc/cuda/cublas_bindings.cu
+++ falkon/csrc/cuda/cublas_bindings.cu
@@ -2,6 +2,7 @@
 
 #include <torch/extension.h>
 #include <c10/cuda/CUDAStream.h>
+#include <c10/cuda/CUDAException.h>
 
 #include <cublas_v2.h>
 #include "utils.cuh"
@@ -48,3 +49,78 @@ void cublas_2d_copy_to_host(const int rows, const int cols, const int elemSize, 
         ldb
     ));
 }
+
+
+void cuda_2d_copy_async(
+    torch::Tensor& dest_tensor,
+    const int dest_pitch,
+    const torch::Tensor& src_tensor,
+    const int src_pitch,
+    const int width,
+    const int height,
+    const at::cuda::CUDAStream &stream
+)
+{
+    C10_CUDA_CHECK(cudaMemcpy2DAsync(
+        dest_tensor.data_ptr(),
+        dest_pitch,
+        src_tensor.data_ptr(),
+        src_pitch,
+        width,
+        height,
+        cudaMemcpyDefault,
+        stream.stream()
+    ));
+}
+
+void cuda_2d_copy( 
+    torch::Tensor& dest_tensor,
+    const int dest_pitch,
+    const torch::Tensor& src_tensor,
+    const int src_pitch,
+    const int width,
+    const int height
+)
+{
+
+    C10_CUDA_CHECK(cudaMemcpy2D(
+        dest_tensor.data_ptr(),
+        dest_pitch,
+        src_tensor.data_ptr(),
+        src_pitch,
+        width,
+        height,
+        cudaMemcpyDefault
+    ));
+}
+
+void cuda_1d_copy_async(
+    torch::Tensor& dest_tensor,
+    const torch::Tensor &src_tensor,
+    const int count,
+    const at::cuda::CUDAStream &stream
+)
+{
+    C10_CUDA_CHECK(cudaMemcpyAsync(
+        dest_tensor.data_ptr(),
+        src_tensor.data_ptr(),
+        count,
+        cudaMemcpyDefault,
+        stream.stream()
+    ));
+}
+
+void cuda_1d_copy(
+    torch::Tensor& dest_tensor,
+    const torch::Tensor &src_tensor,
+    const int count
+)
+{
+    C10_CUDA_CHECK(cudaMemcpy(
+        dest_tensor.data_ptr(),
+        src_tensor.data_ptr(),
+        count,
+        cudaMemcpyDefault
+    ));
+}
+
--- falkon/csrc/cuda/cublas_bindings.h
+++ falkon/csrc/cuda/cublas_bindings.h
@@ -38,3 +38,34 @@ void cublas_2d_copy_to_host(
     const int lda, torch::Tensor& host_tensor,
     const int ldb);
 
+void cuda_2d_copy_async(
+    torch::Tensor& dest_tensor,
+    const int dest_pitch,
+    const torch::Tensor& src_tensor,
+    const int src_pitch,
+    const int width,
+    const int height,
+    const at::cuda::CUDAStream &stream
+);
+
+void cuda_2d_copy(
+    torch::Tensor& dest_tensor,
+    const int dest_pitch,
+    const torch::Tensor& src_tensor,
+    const int src_pitch,
+    const int width,
+    const int height
+);
+
+void cuda_1d_copy_async(
+    torch::Tensor& dest_tensor,
+    const torch::Tensor &src_tensor,
+    const int count,
+    const at::cuda::CUDAStream &stream
+);
+
+void cuda_1d_copy(
+    torch::Tensor& dest_tensor,
+    const torch::Tensor &src_tensor,
+    const int count
+);
